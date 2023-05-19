45047564ab11157ce7af86bb439896f25050c88e
Walter F. Campillay P <walterfabian.cp@hotmail.com>
2023-05-19 12:08:01 -0300

    agrego header, footer y switch color theme

--- gestor.js
+++ gestor.js
@@ -1,3 +1,20 @@
+//switch theme
+const preferedColorScheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+const slider = document.querySelector("#slider-tema");
+
+const setTheme = (theme) => {
+	document.documentElement.setAttribute("data-theme", theme);
+	localStorage.setItem("theme", theme);
+};
+
+slider.addEventListener("click", () => {
+	let switchToTheme = localStorage.getItem("theme") === "dark" ? "light" : "dark";
+	setTheme(switchToTheme);
+});
+
+setTheme(localStorage.getItem("theme") || preferedColorScheme);
+//
+
 //etiquetas del HTML
 const cajaAgregarTarea = document.querySelector(".agregar-tarea");
 const listaDeTareas = document.querySelector(".lista-tareas");
